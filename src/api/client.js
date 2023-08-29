27e56fa55b0cf610fa56eb12e660a53e0bf23613
Boubacar Traore <bt.boubacar@gmail.com>
2023-08-29 22:47:36 +0200

    Testing payment cors issue

--- src/api/client.js
+++ src/api/client.js
@@ -2,8 +2,7 @@ import axios from "axios";
 
 const apiClient = axios.create({
   // baseURL: "https://lereacteur-vinted-api.herokuapp.com",
-  // baseURL: "http://localhost:3000",
-  baseURL: "https://bt-vinted-frontend.netlify.app",
+  baseURL: "http://localhost:3000",
 });
 
 export default apiClient;
